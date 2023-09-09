e9427cff91d2781ae6dffd82f2361e4fa4e6877f
Amitkumarmohanty11 <mohantyamitkumar84@gmail.com>
2023-09-09 07:49:50 +0530

    Java_Literals

---
+++ src/com/company/CWH_04_literals.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class CWH_04_literals {
+
+public static void main(String[] arg) {
+
+    int age = 24;
+    short a = 51;
+    boolean b = true;
+    char x = 'A';
+    byte f =26;
+    double d = 76.9d;
+    float G = 67.9f;
+    long lg = 7876L;
+    String akm = "Amit kumar";
+
+    System.out.println(akm);
+
+}
+
+
+
+
+}
