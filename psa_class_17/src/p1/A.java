3a5ca33a1bceed46596e3ded6154aa14a9896bf7
Amit Kumar Mohanty <105871364+Amitkumarmohanty11@users.noreply.github.com>
2023-11-09 22:52:52 +0530

    Add files via upload

---
+++ psa_class_17/src/p1/A.java
@@ -0,0 +1,19 @@
+package p1;
+
+public class A {
+	
+	public void test() {
+		System.out.println(100);
+	}
+	
+	public void test(int x) {
+		System.out.println(x);
+	}
+	
+	public static void main(String[] args) {
+		A a1 = new A();
+		a1.test();
+		a1.test(200);
+	}
+
+}
