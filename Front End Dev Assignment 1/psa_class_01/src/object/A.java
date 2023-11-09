8b1b6161947ea7d9341a958165488115806032ad
Amit Kumar Mohanty <105871364+Amitkumarmohanty11@users.noreply.github.com>
2023-11-09 22:51:07 +0530

    Add files via upload

---
+++ Front End Dev Assignment 1/psa_class_01/src/object/A.java
@@ -0,0 +1,20 @@
+package object;
+
+public class A {
+	
+	public static void main(String[] args) {
+		
+		A a1 = new A();
+		System.out.println(a1);
+		
+		A a2 = new A();
+		System.out.println(a2);
+		
+		A a3 = new A();
+		System.out.println(a3);
+		
+		A a4 = new A();
+		System.out.println(a4);
+	}
+
+}
---
+++ Front End Dev Assignment 1/psa_class_01/src/topic_new/A.java
@@ -0,0 +1,9 @@
+package topic_new;
+
+public class A {
+	public static void main(String[] args) {
+		
+		new A();
+	}
+
+}
